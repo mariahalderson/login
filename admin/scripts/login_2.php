e977060546aaa9e798e767599df12f59999248cc
Mariah Alderson <mariah@Mariahs-MacBook-Pro.local>
2019-02-03 20:15:30 -0500

    verify user

--- admin/scripts/login_2.php
+++ admin/scripts/login_2.php
@@ -1,37 +1,22 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = 'root';
-$db = 'db_login';
-$conn = mysqli_connect($host, $user, $password, $db); // This order is important
-// for windows add this line - mysqli_set_charset($conn, 'utf8');
-if(!$conn) {
-    echo "something broke ... connection isn't working";
-    exit;
-}
-// echo "connected";
-// go and get some data from the database
-// $myQuery = "SELECT * FROM mainmodel";
-// $result = mysqli_query($conn, $myQuery);
-// $rows = array();
-// //fill the array with the result set and send it to the browser
-// while($row = mysqli_fetch_assoc($result)) {
-//     $rows[] = $row;
-// }
-// get one item from the database
-if(isset($_GET["user"])) {
-    $un = $_GET["user"];
-    $myQuery = "SELECT * FROM tbl_users WHERE user_name='$un'";
-    $result = mysqli_query($conn, $myQuery);
-    $rows = array();
-    //fill the array with the result set and send it to the browser
-    while($row = mysqli_fetch_assoc($result)) {
-    $rows[] = $row;
-}
-}
-//route: http://localhost:8888/a2_week_AJAX/includes/connect.php?modelNo=R58
-//encode the result and send it back
 
-header("Access-Control-Allow-Origin: *");
- header('Content-Type:application/json;charset=UTF-8');
-return json_encode($rows);
+// echo password_hash('rasmuslerdorf', PASSWORD_DEFAULT)."\n";
+//
+// $hash = '$2y$10$M0qW46ra3Nttf3tVDxtC4u.b5JAiua/SPSFYX.ZTb1eTqDCBuytBW';
+//
+// if (password_verify('rasmuslerdorf', $hash)) {
+//     echo 'Password is valid!';
+// } else {
+//     echo 'Invalid password.';
+// }
+include('connect.php');
+
+$pw = $_GET['pw'];
+
+if(password_verify($pw, $hash)){
+  echo 'Password valid!!';
+}else{
+  echo 'something has gone horribly wrong';
+}
+
+?>
--- js/templates/Login.js
+++ js/templates/Login.js
@@ -8,31 +8,49 @@ export default{
       <input type="password" name="password" placeholder="password" v-model="input.password" required>
       <button class="button" type="button" v-on:click="login()">LOGIN</button>
     </form>
+    <p>{{message}}</p>
   </div>
   `,
 
   data(){
     return{
       input:{username:'', password:''},
-      stuff: []
+      stuff: ['empty'],
+      loginAttempts: 0,
+      message: ''
     }
   },
 
   methods:{
     login(){
       console.log("logging in");
-      let url = "./admin/scripts/login.php?user=mariah";
+      let url = "./admin/scripts/login.php?user="+this.input.username;
       console.log(url);
 
       fetch(url)
       .then(res => res.json())
       .then(data => {
-        this.stuff = data[0];
-        console.log(this.stuff);
+        if (data == false || data[0].length < 0) {
+          console.log("Authentication failed, try again");
+          this.loginAttempts++;
+          this.lockAccount();
+        } else {
+          this.stuff = data[0];
+          console.log(this.stuff);
+          this.message = "welcome, "+this.stuff.user_name;
+        }
       })
       .catch(function(error) {
         console.log(error);
       });
+    },
+
+    lockAccount(){
+      if(this.loginAttempts > 2){
+        this.message = "locked out!!";
+      }else{
+        this.message = "attempts: "+this.loginAttempts;
+      }
     }
-}
+  }
 }
